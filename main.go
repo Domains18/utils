760492ef36126d7b584cd73090256775b969e7e3
Domains18 <gibsonsgibson88@gmail.com>
2023-02-27 23:58:08 +0300

    go lang

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
-import(
+import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
-
 func helloHandler(w http.ResponseWriter, r *http.Request){
 	if r.URL.Path != "/hello"{
 		http.Error(w, "404 nor found", http.StatusNotFound)
@@ -33,13 +33,14 @@ func formHandler(w http.ResponseWriter, r *http.Request){
 
 
 func main(){
+	var port int = 3000
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
-	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err!= nil{
+	fmt.Println("Starting server at port", port)
+	if err := http.ListenAndServe(":" + strconv.Itoa(port), nil); err!= nil{
 		log.Fatal(err)
 	}
 }
