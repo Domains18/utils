1b2167007a45c3c16bf104f1341e8a93ab711029
Domains18 <gibsonsgibson88@gmail.com>
2024-05-02 11:37:05 +0000

    May 2, 2024, 2:37 PM

--- pkg/node/requireNode.go
+++ pkg/node/requireNode.go
@@ -1,1 +1,43 @@
 package pkg
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+
+
+type express struct{
+	projectName string
+	inntializeGit bool
+}
+
+
+func CreateExpressProject (projectName string, inntializeGit bool) express{
+	project := express{ projectName: projectName, inntializeGit: inntializeGit}
+
+	err := os.Mkdir(projectName, os.ModePerm)
+	if err != nil {
+		fmt.Printf("error initializing the project : %v\n", err)
+		os.Exit(1)
+	}
+
+	err = os.Chdir(projectName)
+	if err != nil {
+		fmt.Printf("error changing directory to project directory %v\n", err)
+	}
+	
+	cmd := exec.Command("npm", " init", "-y")
+	err = cmd.Run()
+	if err != nil{
+		fmt.Printf("error initializing ")
+	}
+
+	return project
+}
+
+
+func RequireNode(){
+
+}
